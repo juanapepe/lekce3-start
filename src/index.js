97ddf5d2b7711747343ebe51835fb659cdad8352
Jana Silhava <jana.silhava@gmail.com>
2019-03-27 10:17:04 +0100

    z hodiny

--- src/index.js
+++ src/index.js
@@ -47,10 +47,84 @@ import Library  from './library';
 /*let kniha = new Book('Wilbur Smith', 'Řeka bohů', 1980);
 kniha.read();*/
 
-let knihovna = new Library();
+/*let knihovna = new Library();
 knihovna.addBook(new Book('Wilbur Smith', 'Řeka bohů 1', 1980));
 knihovna.addBook(new Book('Wilbur Smith', 'Řeka bohů 2', 1980));
 knihovna.listAllBooks();
  
 knihovna.startReadingNextBook();
-knihovna.listAllBooks();
+knihovna.listAllBooks();*/
+
+/*fetch('https://swapi.co/api/people/1/').then(
+  response => {
+    console.log(response);
+  }
+);*/
+
+console.log('hahaha');
+
+let slib = fetch('https://swapi.co/api/people/1/'); // ulozi promise - ze js da vedet az se provede
+//then reakce na to ze se stane slib
+
+// slib.then(function(vysledek){})
+// fce ktera se ma vykonat kdyz se akce povede
+// fce ktera se ma vykonat kdyz se akce nepovede
+console.log(slib);
+
+slib.then(vysledek => {
+    if (vysledek.ok) {
+    console.log(slib);
+    console.log(vysledek);
+} else {
+    console.log('Vysledek neexistuje');
+}
+});
+
+console.log('hahaha2');
+
+slib.catch(chyba => {
+    console.log(chyba);
+})
+// jiny zpusob zapisu
+slib.catch(() => {
+    console.log(chyba);
+})
+
+slib.then(response => {
+    console.log('Odpoved prisla');
+})
+// jiny zpusob zapisu
+slib.then(() => {
+    console.log('Odpoved prisla');
+})
+
+// consumers - neco co konzumuje slib
+// vysledek do promenne slib a muzeme pouzivat ale nejcasteji se to neuklada
+fetch('https://swapi.co/api/people/1/').then(
+  vysledek => {
+      return vysledek.json(); //text(); // vysledek ve forme textu
+    if (vysledek.ok) {
+        console.log(slib);
+        console.log(vysledek);
+    } else {
+        console.log('Vysledek neexistuje');
+    }
+  }
+).then(obsah => {
+    console.log(obsah.name); // zde mam data
+})
+.catch(() => {
+    console.log(chyba); // toto proved kdyz dojde k chybe
+});
+
+// abych dostal data tak musim rict v jakem formatu data chci
+
+// uvnitr json objectu je pole a tam vlastnost results
+/*let lidi = osoby.results;
+lidi.forEach(clovek => {
+    console.log(clovek.name);
+    
+});*/
+
+// https://home.openweathermap.org/api_keys
+// konec videa pocasi
